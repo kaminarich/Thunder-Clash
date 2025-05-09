e9750ce2c67309a20ca787117c66c0bfa60d9b06
Muhammad Guntur <itawanri13@gmail.com>
2025-05-10 02:02:46 +0700

    third commit

--- set_powersave.sh
+++ set_powersave.sh
@@ -8,5 +8,10 @@ reset_gpu_default
 clear_cache    
 encore_mediatek_powersave
 # Tunggu 5 menit
-sleep 300
-kill_background_apps
+sleep 10
+# Cek ulang apakah layar masih mati
+if ! dumpsys power | grep -iq "mHoldingDisplaySuspendBlocker=true"; then
+    kill_background_apps
+else
+    log "Layar sudah nyala, batal kill app"
+fi
